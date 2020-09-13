0981f3638de37f818a65c8fed6f1b689da3b8986
aniruddhparwal <aniruddhparwal@gmail.com>
2020-09-13 21:39:35 +0530

    Header Completed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './CSS/style.css';
 
 function App() {
 
-  const [countries, setContries] = useState([])
+  const [countries, setContries] = useState([]);
+  const [country, setCountry] = useState("worldwide")
 
   useEffect(() => {
     const getCountriesData = async () => {
@@ -23,15 +24,22 @@ function App() {
     getCountriesData()
   }, [])
 
+  const onCountryChange = (event) => {
+    const countryCode = event.target.value;
+    setCountry(countryCode)
+  }
+
   return (
     <div className="app">
       <div className="app__header">
         <h1>COVID19-TRACKER</h1>
         <FormControl className="app__dropdown">
           <Select
-            varien="outlined"
-            value="abc"
+            variant="outlined"
+            value={country}
+            onChange={onCountryChange}
           >
+            <MenuItem value="worldwide">Worldwide</MenuItem>
             {countries.map((country) => { return (<MenuItem value={country.value}>{country.name}</MenuItem>) })}
           </Select>
         </FormControl>
