2a73ba1c695dd95b0b7c3a3e4bfc7f5b9416ab81
aniruddhparwal <aniruddhparwal@gmail.com>
2020-09-13 21:30:37 +0530

    Drop Down list completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-import React from 'react';
-import './App.css';
+import { FormControl, MenuItem, Select } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import './CSS/style.css';
 
 function App() {
-  return (
-    <div className="App">
 
+  const [countries, setContries] = useState([])
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => (
+            {
+              name: country.country,
+              value: country.countryInfo.iso2,
+            }
+          ));
+          setContries(countries)
+        })
+    }
+    getCountriesData()
+  }, [])
+
+  return (
+    <div className="app">
+      <div className="app__header">
+        <h1>COVID19-TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select
+            varien="outlined"
+            value="abc"
+          >
+            {countries.map((country) => { return (<MenuItem value={country.value}>{country.name}</MenuItem>) })}
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
