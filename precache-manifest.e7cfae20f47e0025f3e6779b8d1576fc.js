199ab6c1cebe52a21b8c41f8945f09816874b245
aniruddhparwal <aniruddhparwal@gmail.com>
2020-09-14 19:53:37 +0530

    Updates

---
+++ precache-manifest.e7cfae20f47e0025f3e6779b8d1576fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95d825bb866cc108a228ec3dafdb084c",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "73d6369668fc45359bcc",
+    "url": "/covid19-tracker/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "7621385bf636db39eaba",
+    "url": "/covid19-tracker/static/css/main.2f2b19a5.chunk.css"
+  },
+  {
+    "revision": "73d6369668fc45359bcc",
+    "url": "/covid19-tracker/static/js/2.13f748cb.chunk.js"
+  },
+  {
+    "revision": "ebc70c097554e9e2ef968c42fad2beba",
+    "url": "/covid19-tracker/static/js/2.13f748cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7621385bf636db39eaba",
+    "url": "/covid19-tracker/static/js/main.fe4d666f.chunk.js"
+  },
+  {
+    "revision": "6b444aef87d753a0bf04",
+    "url": "/covid19-tracker/static/js/runtime-main.815270a9.js"
+  }
+]);
